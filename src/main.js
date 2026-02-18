825de0080c980041c656e0f7dfd39bd7e0fe91b5
Ryn Stewart <rynstwrt17@gmail.com>
2026-02-18 16:11:43 -0600

    Update

--- src/main.js
+++ src/main.js
@@ -1,20 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createWebHistory, createRouter } from "vue-router";
-import ShowFilePage from "./views/ShowFilePage.vue";
+import { createWebHistory, createWebHashHistory, createRouter } from "vue-router";
+import FileRedirectPage from "./views/FileRedirectPage.vue";
 import HomePage from "./views/HomePage.vue";
 
 
 
 const routes = [
     { path: "/", component: HomePage },
-    { path: "/resume", component: ShowFilePage, props: { path: "/RynStewartResume.pdf" } },
-    { path: "/portfolio", component: ShowFilePage, props: { path: "/RynStewartPortfolio.pdf" } }
+    { path: "/test", redirect: "/RynStewartResume.pdf" },
+    { path: "/resume", component: FileRedirectPage, props: { path: "/RynStewartResume.pdf" } },
+    { path: "/portfolio", component: FileRedirectPage, props: { path: "/RynStewartPortfolio.pdf" } }
 ];
 
 
 const router = createRouter({
     history: createWebHistory(),
+    // history: createWebHashHistory(),
     routes
 })
 
