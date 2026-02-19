af493f77e027f5cc74e65d50738360a05d3ff7bf
Ryn Stewart <rynstwrt17@gmail.com>
2026-02-18 21:49:14 -0600

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,154 @@
-<template>
-    <RouterView />
-</template>
+<script setup>
+import "./App.scss";
+import { onMounted } from "vue";
+import { tsParticles } from "@tsparticles/engine";
+import { loadSlim } from "@tsparticles/slim";
+import particlesConfig from "./assets/json/particles-config.json";
+import { createTimeline, splitText, stagger } from "animejs";
+import LandingPage from "./views/LandingPage.vue";
+// import RynFooter from "../components/RynFooter.vue";
+// import Project from "../components/Project.vue";
 
+
+function loadBackgroundParticles() {
+    (async engine => {
+        await loadSlim(engine);
+
+        await engine.load({
+            id: "bg-particles",
+            options: particlesConfig
+        });
+    })(tsParticles);
+}
+
+
+function createNameAnimation() {
+    const { words, chars } = splitText("#landing h1", {
+        words: true,
+        chars: true
+    });
+
+
+    createTimeline({
+        defaults: {
+            duration: 600,
+            ease: 'inOut', // ease applied between each keyframes if no ease defined
+        },
+        delay: 150,
+    })
+    .add(words, {
+        y: "-=0.3lh",
+        ease: 'outCubic'
+    }, stagger(100))
+    .add(chars, {
+        duration: 650,
+
+        scale: [
+            { to: 1.123, ease: "in" },
+            { to: 1, ease: "out" }
+        ],
+
+        rotateX: {
+            to: "-1turn",
+            ease: "inOutSine",
+            delay: stagger(35),
+        }
+    }, 0)
+    .add(words, {
+        y: 0,
+        ease: 'outQuad',
+        duration: 400
+    }, stagger(150, { reversed: true }));
+}
+
+
+onMounted(() => {
+    loadBackgroundParticles();
+    createNameAnimation();
+});
+</script>
+
+
+
+<template>
+    <!-- BACKGROUND PARTICLES -->
+    <div id="bg-particles" ref="bg-particles"></div>
+
+
+    <!-- LANDING PAGE SECTION -->
+    <LandingPage />
+
+
+    <!--<section id="code">-->
+    <!--    <h2>Code</h2>-->
+
+    <!--    <Project-->
+    <!--        title="Chord Progression Generator"-->
+    <!--        desc="Generate chord progressions."-->
+    <!--        url="https://quickbin.app"-->
+    <!--        repo="https://github.com/rynstwrt/quickbin.app"-->
+    <!--        image="ChordProgressionGenerator.jpg" />-->
+
+    <!--    <Project-->
+    <!--        title="Quickbin"-->
+    <!--        desc="Like Pastebin, but quicker. Saved quickbins are read-only."-->
+    <!--        url="https://quickbin.app"-->
+    <!--        repo="https://github.com/rynstwrt/quickbin.app"-->
+    <!--        image="Quickbin.png" />-->
+
+    <!--    <Project-->
+    <!--        title="EditorPerRepo"-->
+    <!--        desc="Description"-->
+    <!--        url="https://"-->
+    <!--        repo="https://github.com/rynstewart/EditorPerRepoElectron" />-->
+
+    <!--    <Project-->
+    <!--        title="DomainDissidence"-->
+    <!--        desc="Description"-->
+    <!--        url="https://domaindissidence.online"-->
+    <!--        repo="https://github.com/DomainDissidence" />-->
+
+    <!--    <Project-->
+    <!--        title="PixelblazeDesktop"-->
+    <!--        desc="Description"-->
+    <!--        url=""-->
+    <!--        repo="https://github.com/rynstwrt/Pixelblaze-Desktop" />-->
+    <!--</section>-->
+
+
+    <!--<section id="websites">-->
+    <!--    <h2>Web Development</h2>-->
+
+    <!--    <Project-->
+    <!--        title="offtheblock.lol"-->
+    <!--        desc="A website design for a Minecraft server."-->
+    <!--        url="https://offtheblock.lol"-->
+    <!--        repo="https://github.com/rynstwrt/offtheblock.lol"-->
+    <!--        image="OffTheBlock.jpg" />-->
+
+    <!--    <Project-->
+    <!--        title="CSS Animation Gallery"-->
+    <!--        desc="Description"-->
+    <!--        url="https://"-->
+    <!--        repo="https://github.com/" />-->
+
+    <!--    <Project-->
+    <!--        title="ilovefoxes.org"-->
+    <!--        desc="Description"-->
+    <!--        url="https://ilovefoxes.org"-->
+    <!--        repo="https://github.com/rynstwrt/ilovefoxes.org" />-->
+    <!--</section>-->
+
+
+
+    <!--<section id="design">-->
+    <!--    <h2>Design</h2>-->
+    <!--</section>-->
+
+
+    <!--&lt;!&ndash; FOOTER &ndash;&gt;-->
+    <!--<RynFooter />-->
+</template>
 
 
 <style lang="scss">
@@ -37,5 +184,13 @@ body {
 a {
     color: inherit;
     text-decoration: none;
+}
+</style>
+
+
+
+<style lang="scss" scoped>
+.website {
+    margin: 20px 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,28 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createWebHistory, createWebHashHistory, createRouter } from "vue-router";
-import FileRedirectPage from "./views/FileRedirectPage.vue";
-import HomePage from "./views/HomePage.vue";
-
-
-
-const routes = [
-    { path: "/", component: HomePage },
-    { path: "/test", redirect: "/RynStewartResume.pdf" },
-    { path: "/resume", component: FileRedirectPage, props: { path: "/RynStewartResume.pdf" } },
-    { path: "/portfolio", component: FileRedirectPage, props: { path: "/RynStewartPortfolio.pdf" } },
-    { path: "/**", component: HomePage }
-];
-
-
-const router = createRouter({
-    history: createWebHistory(),
-    // history: createWebHashHistory(),
-    routes
-})
 
 
 createApp(App)
-.use(router)
 // .use(VueSplide)
 .mount('#app');
