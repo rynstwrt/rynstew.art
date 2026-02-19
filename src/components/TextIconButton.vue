60f5b8cc9db0effe89742f0b0bbb5cf36e51a292
Ryn Stewart <rynstwrt17@gmail.com>
2026-02-19 12:00:44 -0600

    Backup

---
+++ src/components/TextIconButton.vue
@@ -0,0 +1,77 @@
+<script setup>
+const props = defineProps([
+    "text",
+    "icon",
+    "url",
+    "center"
+]);
+
+const openURL = url => window.open(url, "_blank");
+</script>
+
+
+
+<template>
+    <button
+        :class="() => 'social-btn' + props.center==='true' ? ' center' : ''"
+        @click="props.url && openURL(props.url)" >
+        <span class="social-icon" v-if="props.icon" v-html="props.icon"></span>
+        <span class="social-text" v-if="props.text">{{ props.text }}</span>
+    </button>
+</template>
+
+
+
+<style lang="scss" scoped>
+button {
+    background-color: transparent;
+    //font-size: inherit;
+    //border: 1px solid $color-light-2;
+    border: 1px solid rgba($color-light-2, 0.75);
+    //border-radius: 2px;
+    padding: 5px 7px;
+    cursor: pointer;
+    opacity: 0.8;
+    transition: opacity .05s ease-in;
+    display: flex;
+    align-items: center;
+    //font-weight: 200;
+
+    &.center {
+        justify-content: center;
+        text-align: center;
+    }
+
+    :deep(span.social-icon) {
+        //display: flex;
+        //justify-content: center;
+        //align-items: center;
+
+        width: 25px;
+        height: 25px;
+        //aspect-ratio: 1;
+        //margin-right: 5px;
+
+        svg {
+            color: $color-primary;
+            height: 100%;
+            width: auto;
+            padding: 2px;
+        }
+    }
+
+    :deep(span.social-text) {
+        color: $color-light;
+        font-weight: 200;
+        //font-size: 0.85rem;
+        font-size: 0.9rem;
+        letter-spacing: 1px;
+        margin-left: 5px;
+    }
+
+    &:hover {
+        opacity: 1;
+        //border-color: $color-light-2;
+    }
+}
+</style>
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -6,22 +6,22 @@ import IconButton from "../components/IconButton.vue";
 
 import LinkedInIcon from "../assets/icon/linkedin.svg?raw";
 import GitHubIcon from "../assets/icon/github.svg?raw";
-import CameraIcon from "../assets/icon/camera.svg?raw";
-import InstagramIcon from "../assets/icon/instagram.svg?raw";
+import UnsplashIcon from "../assets/icon/unsplash.svg?raw";
+import TextIconButton from "../components/TextIconButton.vue";
 import ResumeIcon from "../assets/icon/resume.svg?raw";
 
 
-const SOCIALS = [
-    { text: 'GitHub', icon: GitHubIcon, url: 'https://github.com/rynstwrt' },
-    { text: 'LinkedIn', icon: LinkedInIcon, url: 'https://linkedin.com/in/rynstwrt' },
-    { text: 'Instagram', icon: InstagramIcon, url: 'https://unsplash.com/rynstwrt' },
-    { text: 'Unsplash', icon: CameraIcon, url: 'https://instagram.com/rynstwrt' }
-];
-
-
-function openResume() {
-    window.open('/RynStewartResume.pdf', '_blank');
-}
+// const SOCIALS = [
+//     { text: 'GitHub', icon: GitHubIcon, url: 'https://github.com/rynstwrt' },
+//     { text: 'LinkedIn', icon: LinkedInIcon, url: 'https://linkedin.com/in/rynstwrt' },
+//     { text: 'Instagram', icon: InstagramIcon, url: 'https://unsplash.com/rynstwrt' },
+//     { text: 'Unsplash', icon: CameraIcon, url: 'https://instagram.com/rynstwrt' }
+// ];
+// /
+//
+// function openResume() {
+//     window.open('/RynStewartResume.pdf', '_blank');
+// }
 </script>
 
 
@@ -34,20 +34,51 @@ function openResume() {
             <p id="byline">Artist and full-stack developer in<br />Dallas, Texas.</p>
 
             <div id="social-grid">
-                <IconButton
-                    v-for="social in SOCIALS"
-                    :text="social.text"
-                    :icon='social.icon.replace(/(?=.*?)>/s, ` preserveAspectRatio="none">`)'
-                    :url="social.url" />
 
                 <IconButton
-                    id="resume-btn"
-                    text="View Resume"
+                    :icon="GitHubIcon"
+                    url="https://github.com/rynstwrt" />
+
+                <IconButton
+                    :icon="LinkedInIcon"
+                    url="https://linkedin.com/in/rynstwrt" />
+
+                <IconButton
+                    :icon="UnsplashIcon"
+                    url="https://unsplash.com/rynstwrt" />
+
+                <!--<IconButton-->
+                <!--    v-for="social in SOCIALS"-->
+                <!--    :text="social.text"-->
+                <!--    :icon='social.icon.replace(/(?=.*?)>/s, ` preserveAspectRatio="none">`)'-->
+                <!--    :url="social.url" />-->
+
+                <!--<IconButton-->
+                <!--    id="resume-btn"-->
+                <!--    text="View Resume"-->
+                <!--    :icon=ResumeIcon-->
+                <!--    :center="true"-->
+                <!--    @click="openResume"-->
+                <!--/>-->
+            </div>
+
+
+            <div id="resume-portfolio-buttons">
+                <TextIconButton
                     :icon=ResumeIcon
-                    :center="true"
-                    @click="openResume"
+                    text="View Resume"
+                    url="/RynStewartResume.pdf"
+                />
+
+                <TextIconButton
+                    :icon=ResumeIcon
+                    text="View Portfolio"
+                    url="/RynStewartPortfolio.pdf"
                 />
             </div>
+
+
+
         </main>
 
         <!--<DownChevron />-->
