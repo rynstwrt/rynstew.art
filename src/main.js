77411e5568d50b20fb28d1847eb4375d42defa3d
Ryn Stewart <rynstwrt17@gmail.com>
2026-02-20 18:30:40 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ const router = createRouter({
         {
             path: "/portfolio", component: PDFPage, props: {
                 url: "/RynStewartPortfolio.pdf",
-                title: "Design Portfolio"
+                title: "Portfolio"
             }
         },
         // {
