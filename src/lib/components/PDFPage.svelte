c46181447d8ad3d45dc509b2e8cbc9a2f8fda994
Ryn Stewart <rynstwrt17@gmail.com>
2026-04-30 09:03:34 -0500

    Update PDFPage.svelte

--- src/lib/components/PDFPage.svelte
+++ src/lib/components/PDFPage.svelte
@@ -2,12 +2,13 @@
     import { HomeOutline } from "flowbite-svelte-icons";
     import LandingButton from "$lib/components/LandingButton.svelte";
     import PDFEmbed from "$lib/components/PDFEmbed.svelte";
+    import { base } from "$app/paths";
 
     let {src} = $props();
 </script>
 
 
-<LandingButton href="/"
+<LandingButton href="{base}/"
                Icon={HomeOutline}
                text="Return Home"
                classes="w-fit place-self-center mt-8 mb-6"/>
