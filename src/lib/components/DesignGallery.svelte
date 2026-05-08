c4fc126794a005c47bc61adb68095fa22fff0f20
Ryn Stewart <rynstwrt17@gmail.com>
2026-05-07 21:09:14 -0500

    Update

---
+++ src/lib/components/DesignGallery.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+    import {
+        Gallery,
+        Carousel,
+        CarouselIndicators,
+        Controls,
+        Thumbnails
+    } from "flowbite-svelte";
+    import { scale } from "svelte/transition";
+    import { quintOut } from "svelte/easing";
+
+    let images = [
+        {alt: "Glitch Headshot", src: "OrangeCyanHeadshot.jpg"},
+        {alt: "Nutriwise Company Logo", src: "NutriwiseLogo.png"},
+        {alt: "Alphabet Designs", src: "AlphabetDesign.png"},
+        {alt: "Glitch Design", src: "glitch1.jpg"},
+        {alt: "Glitch Design 2", src: "glitch2.jpg"},
+        {alt: "Glitch Design 3", src: "glitch3.jpg"},
+        {alt: "Letter R Design 1", src: "r_designs/1.png"},
+        {alt: "Letter R Design 2", src: "r_designs/2.png"},
+        {alt: "Letter R Design 3", src: "r_designs/3.png"},
+        {alt: "Letter R Design 4", src: "r_designs/4.png"},
+        {alt: "ScurryDog Company Logo", src: "ScurryDog.png"},
+        {alt: "Letter R Design 5", src: "r_designs/5.png"},
+        {alt: "Letter R Design 6", src: "r_designs/6.png"},
+        {alt: "Letter R Design 7", src: "r_designs/7.png"},
+        {alt: "Letter R Design 8", src: "r_designs/8.png"},
+        {alt: "Letter R Design 9", src: "r_designs/9.png"},
+    ];
+
+    images.forEach(image => image.src = `/src/lib/images/portfolio/${image.src}`)
+
+    let index = $state(0);
+
+    const transition = (node: HTMLElement) => scale(node, { duration: 500, easing: quintOut });
+
+</script>
+
+
+<!--<div class="max-w-4xl space-y-4">-->
+<!--    <Carousel bind:index={designPortfolioIdx}-->
+<!--              images={designPortfolioImages}>-->
+<!--        <Controls/>-->
+<!--        <CarouselIndicators/>-->
+<!--    </Carousel>-->
+<!--    <Thumbnails images={designPortfolioImages} bind:index={designPortfolioIdx}/>-->
+<!--</div>-->
+
+
+<div class="max-w-4xl m-3">
+    <Carousel {images} {transition} bind:index class="min-h-full">
+        <Controls />
+        <!--<CarouselIndicators />-->
+    </Carousel>
+
+    <Thumbnails {images} bind:index class="bg-transparent" />
+
+</div>
--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,39 +1,16 @@
 <script lang="ts">
-    import { Tabs, TabItem, Gallery } from "flowbite-svelte";
-    import { GlobeSolid, HomeOutline, PenNibSolid } from "flowbite-svelte-icons";
+    import {
+        Tabs,
+        TabItem,
+    } from "flowbite-svelte";
+    import {
+        GlobeSolid,
+        HomeOutline,
+        PenNibSolid
+    } from "flowbite-svelte-icons";
     import { resolve } from "$app/paths";
     import BoxButton from "$lib/components/BoxButton.svelte";
-
-    const imageCol1 = [
-        {alt: "Glitch Headshot", src: "OrangeCyanHeadshot.jpg"},
-        {alt: "Nutriwise Company Logo", src: "NutriwiseLogo.png"},
-        {alt: "Alphabet Designs", src: "AlphabetDesign.png"},
-        {alt: "Glitch Design", src: "glitch1.jpg"},
-        {alt: "Glitch Design 2", src: "glitch2.jpg"},
-    ]
-
-    const imageCol2 = [
-        {alt: "Glitch Design 3", src: "glitch3.jpg"},
-        {alt: "Letter R Design 1", src: "r_designs/1.png"},
-        {alt: "Letter R Design 2", src: "r_designs/2.png"},
-        {alt: "Letter R Design 3", src: "r_designs/3.png"},
-        {alt: "Letter R Design 4", src: "r_designs/4.png"},
-    ]
-
-    const imageCol3 = [
-        {alt: "ScurryDog Company Logo", src: "ScurryDog.png"},
-        {alt: "Letter R Design 5", src: "r_designs/5.png"},
-        {alt: "Letter R Design 6", src: "r_designs/6.png"},
-        {alt: "Letter R Design 7", src: "r_designs/7.png"},
-        {alt: "Letter R Design 8", src: "r_designs/8.png"},
-        // {alt: "Letter R Design 9", src: "r_designs/9.png"},
-    ]
-
-    for (const imageCol of [imageCol1, imageCol2, imageCol3]) {
-        for (const image of imageCol) {
-            image.src = `/src/lib/images/portfolio/${image.src}`
-        }
-    }
+    import DesignGallery from "$lib/components/DesignGallery.svelte";
 </script>
 
 
@@ -43,30 +20,29 @@
            classes="w-fit place-self-center mt-8 mb-6"/>
 
 
-<div class="p-3">
-    <Tabs tabStyle="full" class="flex divide-x shadow-sm rtl:divide-x-reverse divide-gray-700">
-        <TabItem class="w-full flex justify-center">
-            {#snippet titleSlot()}
-                <div class="flex justify-center items-center gap-2">
-                    <GlobeSolid size="md"/>
-                    Websites
-                </div>
-            {/snippet}
-            <p>WEBSITES</p>
-        </TabItem>
+<DesignGallery />
 
-        <TabItem open class="w-full">
-            {#snippet titleSlot()}
-                <div class="flex justify-center items-center gap-2">
-                    <PenNibSolid size="md"/>
-                    Design
-                </div>
-            {/snippet}
-            <Gallery class="grid-cols-2 md:grid-cols-3 gap-4">
-                <Gallery items={imageCol1}/>
-                <Gallery items={imageCol2}/>
-                <Gallery items={imageCol3}/>
-            </Gallery>
-        </TabItem>
-    </Tabs>
-</div>
+
+<!--<div class="p-3 h-full">-->
+    <!--<Tabs tabStyle="full" class="flex divide-x shadow-sm rtl:divide-x-reverse divide-gray-700">-->
+    <!--    <TabItem class="w-full">-->
+    <!--        {#snippet titleSlot()}-->
+    <!--            <div class="flex justify-center items-center gap-2">-->
+    <!--                <GlobeSolid size="md"/>-->
+    <!--                Websites-->
+    <!--            </div>-->
+    <!--        {/snippet}-->
+    <!--        <p>WEBSITES</p>-->
+    <!--    </TabItem>-->
+
+    <!--    <TabItem open class="w-full">-->
+    <!--        {#snippet titleSlot()}-->
+    <!--            <div class="flex justify-center items-center gap-2">-->
+    <!--                <PenNibSolid size="md"/>-->
+    <!--                Design-->
+    <!--            </div>-->
+    <!--        {/snippet}-->
+    <!--        <p>DESIGN</p>-->
+    <!--    </TabItem>-->
+    <!--</Tabs>-->
+<!--</div>-->
