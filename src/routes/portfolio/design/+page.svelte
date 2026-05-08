8f9fb4a7bd77bbccdb59f058e38f178f11581cb1
Ryn Stewart <rynstwrt17@gmail.com>
2026-05-08 14:02:50 -0500

    Reorganize images

--- src/routes/portfolio/design/+page.svelte
+++ src/routes/portfolio/design/+page.svelte
@@ -8,25 +8,23 @@
     import PortfolioGallery from "$lib/components/PortfolioGallery.svelte";
 
     let images = [
-        {alt: "Glitch Headshot", src: "OrangeCyanHeadshot.jpg", width: 1500, height: 1500},
-        {alt: "Nutriwise Company Logo", src: "NutriwiseLogo.png", width: 1000, height: 1000},
-        {alt: "Alphabet Designs", src: "AlphabetDesign.png", width: 4200, height: 2100},
-        {alt: "Glitch Design", src: "glitch1.jpg", width: 2048, height: 2048},
-        {alt: "Glitch Design 2", src: "glitch2.jpg", width: 2048, height: 2048},
-        {alt: "Glitch Design 3", src: "glitch3.jpg", width: 2048, height: 2048},
-        {alt: "Letter R Design 1", src: "r_designs/1.png", width: 450, height: 450},
-        {alt: "Letter R Design 2", src: "r_designs/2.png", width: 450, height: 450},
-        {alt: "Letter R Design 3", src: "r_designs/3.png", width: 450, height: 450},
-        {alt: "Letter R Design 4", src: "r_designs/4.png", width: 450, height: 450},
-        {alt: "ScurryDog Company Logo", src: "ScurryDog.png", width: 1500, height: 1500},
-        {alt: "Letter R Design 5", src: "r_designs/5.png", width: 450, height: 450},
-        {alt: "Letter R Design 6", src: "r_designs/6.png", width: 450, height: 450},
-        {alt: "Letter R Design 7", src: "r_designs/7.png", width: 450, height: 450},
-        {alt: "Letter R Design 8", src: "r_designs/8.png", width: 450, height: 450},
-        // {alt: "Letter R Design 9", src: "r_designs/9.png", width: 450, height: 450},
+        {alt: "Glitch Headshot", src: "/src/lib/images/portfolio/design/OrangeCyanHeadshot.jpg", width: 1500, height: 1500},
+        {alt: "Nutriwise Company Logo", src: "/src/lib/images/portfolio/design/NutriwiseLogo.png", width: 1000, height: 1000},
+        {alt: "Alphabet Designs", src: "/src/lib/images/portfolio/design/AlphabetDesign.png", width: 4200, height: 2100},
+        {alt: "Glitch Design", src: "/src/lib/images/portfolio/design/glitch1.jpg", width: 2048, height: 2048},
+        {alt: "Glitch Design 2", src: "/src/lib/images/portfolio/design/glitch2.jpg", width: 2048, height: 2048},
+        {alt: "Glitch Design 3", src: "/src/lib/images/portfolio/design/glitch3.jpg", width: 2048, height: 2048},
+        {alt: "Letter R Design 1", src: "/src/lib/images/portfolio/design/r_designs/1.png", width: 450, height: 450},
+        {alt: "Letter R Design 2", src: "/src/lib/images/portfolio/design/r_designs/2.png", width: 450, height: 450},
+        {alt: "Letter R Design 3", src: "/src/lib/images/portfolio/design/r_designs/3.png", width: 450, height: 450},
+        {alt: "Letter R Design 4", src: "/src/lib/images/portfolio/design/r_designs/4.png", width: 450, height: 450},
+        {alt: "ScurryDog Company Logo", src: "/src/lib/images/portfolio/design/ScurryDog.png", width: 1500, height: 1500},
+        {alt: "Letter R Design 5", src: "/src/lib/images/portfolio/design/r_designs/5.png", width: 450, height: 450},
+        {alt: "Letter R Design 6", src: "/src/lib/images/portfolio/design/r_designs/6.png", width: 450, height: 450},
+        {alt: "Letter R Design 7", src: "/src/lib/images/portfolio/design/r_designs/7.png", width: 450, height: 450},
+        {alt: "Letter R Design 8", src: "/src/lib/images/portfolio/design/r_designs/8.png", width: 450, height: 450},
+        // {alt: "Letter R Design 9", src: "/src/lib/images/portfolio/design/r_designs/9.png", width: 450, height: 450},
     ];
-
-    images.forEach(image => image.src = `/src/lib/images/portfolio/${image.src}`)
 </script>
 
 
