812cfcb237ce83260ca5f6663c3e57fe3cc570b9
Ryn Stewart <rynstwrt17@gmail.com>
2026-05-07 20:03:52 -0500

    Backup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
     import './layout.css';
 
+    import {
+        Navbar,
+        NavBrand,
+        NavHamburger,
+        NavLi,
+        NavUl
+    } from "flowbite-svelte";
+
     import ParticleBg from "$lib/components/ParticleBG.svelte";
+    import { resolve } from "$app/paths";
+    import { page } from "$app/state";
 
     let {children} = $props();
+
+    let activeUrl = $derived(page.url.pathname);
 </script>
 
 
@@ -20,4 +32,19 @@
 
 
 <ParticleBg/>
+
+<Navbar>
+    <NavBrand href={resolve("/")}>
+        <img src="/favicon.svg" alt="Logo" class="me-2 h-6" />
+        <span class="text-lg font-medium whitespace-nowrap">rystew.art</span>
+    </NavBrand>
+    <NavHamburger/>
+    <NavUl {activeUrl}>
+        <NavLi href={resolve("/")}>Home</NavLi>
+        <NavLi href={resolve("/resume")}>Resume</NavLi>
+        <NavLi href={resolve("/portfolio")}>Design</NavLi>
+        <NavLi href={resolve("/websites")}>Websites</NavLi>
+    </NavUl>
+</Navbar>
+
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,9 @@
     import {
         GithubSolid,
         FileLinesOutline,
-        LinkedinSolid
+        LinkedinSolid,
+        GlobeOutline,
+        SwatchbookOutline
     } from "flowbite-svelte-icons";
 
     import BoxButton from "$lib/components/BoxButton.svelte";
@@ -16,29 +18,51 @@
         <h1 class="text-[3.5rem] font-medium  uppercase text-accent">Ryn<br/>Stewart</h1>
         <p class="text-lg font-light font-source-code-pro mt-1 leading-6">Artist and developer in Dallas, Texas.</p>
 
-        <div id="social-grid" class="grid mt-3 gap-1.5">
+
+        <div id="social-grid" class="grid gap-1.5 mt-3">
             <BoxButton href="https://linkedin.com/in/rynstwrt"
                        target="_blank"
                        Icon={LinkedinSolid}
-                       classes="aspect-square justify-center"/>
+                       classes="w-fit aspect-square justify-center"/>
+
             <BoxButton href="https://github.com/rynstwrt"
                        target="_blank"
                        Icon={GithubSolid}
-                       classes="aspect-square justify-center"/>
-            <BoxButton href={resolve("/portfolio")}
-                       Icon={FileLinesOutline}
-                       text="View Portfolio"/>
+                       classes="w-fit aspect-square justify-center"/>
+
+            <BoxButton href={resolve("/websites")}
+                       Icon={GlobeOutline}
+                       text="Websites"/>
+
             <BoxButton href={resolve("/resume")}
                        Icon={FileLinesOutline}
                        text="View Resume"
                        classes="col-span-full"/>
+            <!--<BoxButton href={resolve("/portfolio")}-->
+            <!--           Icon={SwatchbookOutline}-->
+            <!--           text="Design Portfolio"/>-->
+
+            <!--<BoxButton href="https://linkedin.com/in/rynstwrt"-->
+            <!--           Icon={LinkedinSolid}-->
+            <!--           text="LinkedIn" />-->
+
+            <!--<BoxButton href="https://github.com/rynstwrt"-->
+            <!--           Icon={GithubSolid}-->
+            <!--           text="GitHub"/>-->
         </div>
+
+        <!--<div class="mt-4">-->
+        <!--    <a href="https://github.com/rynstwrt" class="block">-->
+        <!--        <GithubSolid class="shrink-0 h-7 w-7 text-gray-300 opacity-80 hover:opacity-100 transition-opacity" />-->
+        <!--    </a>-->
+        <!--</div>-->
     </div>
 </main>
 
 
 <style>
     #social-grid {
+        /*grid-template-columns: auto 1fr 1fr;*/
         grid-template-columns: auto auto 1fr;
     }
 </style>
