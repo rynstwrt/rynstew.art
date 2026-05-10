75a99cae98322e505d9ee057dd4b0a3d82fc64d2
Ryn Stewart <rynstwrt17@gmail.com>
2026-05-10 15:32:16 -0500

    Start fleshing out websites section

--- src/routes/websites/+page.svelte
+++ src/routes/websites/+page.svelte
@@ -1,43 +1,50 @@
 <script lang="ts">
+    import { Button, Heading, P, Card } from "flowbite-svelte";
 
     const websites = [
         {
-            // title: "andrewselectronics.us",
             title: "Andrew's Electronics",
             description: "A website created for Andrew's Electronics, a vintage electronics repair business.",
             href: "https://rynstwrt.github.io/andrewselectronics.us",
-            hrefTitle: "andrewselectronics.us",
             img: "/images/websites/andrewselectronics.png"
         },
         {
-            title: "",
-            description: "",
-            href: "",
-            hrefTitle: "",
-            img: ""
+            title: "Off The Block",
+            description: "A website created for a Minecraft server.",
+            href: "https://offtheblock.lol",
+            img: "/images/websites/offtheblock.png"
         }
-    ]
+    ];
+
+    // let imgRefs = $state([]);
 </script>
 
 
-<p class="text-center mt-10 text-xl font-light">Coming soon.</p>
+<!--<p class="text-center mt-10 text-xl font-light">Coming soon.</p>-->
 
+<div class="flex flex-col items-center mt-2 px-4 gap-3">
+<!--<div class="flex flex-col items-center mt-2 px-4">-->
+    {#each websites as website}
+        <Card class="p-4 sm:p-6 md:p-6 w-80">
+            <Heading tag="h2"
+                     class="mb-2 text-xl leading-none font-semibold text-gray-900 md:text-2xl">{website.title}</Heading>
 
-<!--{#each websites as website}-->
-<!--    <div class="bg-gray-800 rounded-md px-4 py-5 m-3 max-w-120">-->
-<!--        <h3 class="text-2xl font-medium tracking-wide">{website.title}</h3>-->
+            <P size="sm" class="text-gray-400!">{website.description}</P>
 
-<!--        <a href={website.href}-->
-<!--           target="_blank"-->
-<!--           class="text-accent text-sm hover:underline block mt-0.5">-->
-<!--            {website.hrefTitle ? website.hrefTitle : website.href}-->
-<!--        </a>-->
+            <enhanced:img src={website.img}
+                          alt={`${website.title} preview`}
+                          class="my-4 rounded-lg  border border-gray-300/10"/>
 
-<!--        <p class="text-sm font-light mt-2">{website.description}</p>-->
+            <div class="flex  items-center space-x-4">
+                <Button href={website.href}
+                        target="_blank"
+                        size="sm"
+                        class="bg-accent px-8">
+                    Visit
+                </Button>
+            </div>
+        </Card>
+    {/each}
+</div>
 
-<!--        <img src={website.img}-->
-<!--             alt={`${website.title} screenshot`}-->
-<!--             class="mt-3 rounded-md shadow-md"/>-->
-<!--    </div>-->
-<!--{/each}-->
-
+<p class="text-center pt-7 pb-12 text-lg font-light">More entries coming soon.</p>
